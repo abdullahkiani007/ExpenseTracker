eb12826368f82b95013eeb78d539e312b57dafd7
Abdul Wahab <abdul.wahab@codistan.org>
2024-04-29 23:42:12 +0500

    new changes

--- App.js
+++ App.js
@@ -13,6 +13,10 @@ import {
 import { SelectList } from "react-native-dropdown-select-list";
 import Expense from "./components/Expense";
 
+import data from "./expense.json";
+
+
+
 
 // App Component
 const App = () => {
@@ -23,82 +27,47 @@ const App = () => {
   const [expenseTitle, setExpenseTitle] = useState("");
   const [expense, setExpense] = useState(0);
   const [totalExpenditure, setTotalExpenditure] = useState(0);
+  const [expenseData, setExpenseData] = useState([]);
+  const [editIndex, setEditIndex] = useState(null);
+
 
   const handleEdit = (index) => {
     setExpenseTitle(expenseData[index].title);
     setExpense(expenseData[index].amount);
-    console.log("Edit", expenseData[index].amount);
+    setEditIndex(index);
+
   }
 
-  const handleTotalExpense = ()=>{
-    const sum = expenseData.reduce((item,index)=>item.amount+index.amount,0)
-    setTotalExpenditure(sum)
-  }
+//  const handleTotalExpense = () => {
+//   const sum = expenseData.reduce((total, item) => total + item.amount, 0);
+//   setTotalExpenditure(sum);
+// };
+
+const handleTotalExpense = () => {
+  let sum = 0;
+  expenseData.forEach((item) => {
+    sum += item.amount;
+  });
+  setTotalExpenditure(sum);
+};
+
   
   const handleDelete = (key)=>{
-    Alert.alert('Alert Title', 'My Alert Msg', [
+    Alert.alert('Delete Expense', 'Are you sure ?', [
       {
         text: 'Cancel',
         onPress: () => console.log('Cancel Pressed'),
         style: 'cancel',
       },
-      {text: 'OK', onPress: () => console.log('OK Pressed')},
+      {text: 'OK', onPress: () => {
+        const newExpenseData = expenseData.filter((item,index)=>key != index)
+        setExpenseData(newExpenseData);
+        handleTotalExpense();
+      }},
     ]);
-    expenseData.filter((item,index)=>key != index)
+   
   }
-  const expenseData = [
-    {
-      "title": "Groceries savemart",
-      "amount": 100,
-      "Date": "May 16,2023",
-      "Time": "1:29 PM"
-    },{
-      "title": "Sports Equipment",
-      "amount": 200,
-      "Date": "May 20,2023",
-      "Time": "2:29 PM"
-    },{
-      "title": "Clothes",
-      "amount": 300,
-      "Date": "May 30,2023",
-      "Time": "3:29 PM"
-    },{
-      "title": "Food",
-      "amount": 400,
-      "Date": "May 10,2023",
-      "Time": "4:29 PM"
-    },{
-      "title": "Electronics",
-      "amount": 500,
-      "Date": "May 12,2023",
-      "Time": "5:29 PM"
-    },{
-      "title": "Furniture",
-      "amount": 600,
-      "Date": "May 15,2023",
-      "Time": "6:29 PM"
-    },{
-      "title": "Books",
-      "amount": 700,
-      "Date": "May 17,2023",
-      "Time": "7:29 PM"
-    },{
-      "title": "Stationary",
-      "amount": 800,
-      "Date": "May 18,2023",
-      "Time": "8:29 PM"
-    },{
-      "title": "Shoes",
-      "amount": 900,
-      "Date": "May 19,2023",
-      "Time": "9:29 PM"
-    },{
-      "title": "Toys",
-      "amount": 1000,
-      "Date": "May 21,2023",
-      "Time": "10:29 PM"
-    }
-  ]
+
   const months = [
     { key: "1", value: "January" },
     { key: "2", value: "February" },
@@ -114,9 +83,10 @@ const App = () => {
     { key: "12", value: "December" },
   ];
 
+  console.log('data' , data)
 
   const getCurrentYear = () => {
-    const currentYear = new Date().getFullYear();
+    const currentYear = new Date().getFullYear()-1;
     const years = [];
     for (let i = 1; i <= 10; i++) {
       years.push({ key: `${i}`, value: `${currentYear + i}` });
@@ -133,13 +103,44 @@ const App = () => {
 
   const handleAddExpense = () => {
     const expenseValue = parseFloat(expense);
-    if (!isNaN(expenseValue)) {
+  
+    if(editIndex !== null){
+      const newExpenseData = expenseData.map((item,index)=>{
+        if(index === editIndex){
+          return {
+            title: expenseTitle,
+            amount: expenseValue,
+            Date: month+" "+year,
+            Time: new Date().toLocaleTimeString()
+          }
+        }
+        return item;
+      })
+      setExpenseData(newExpenseData);
+      setTotalExpenditure(totalExpenditure - expenseData[editIndex].amount + expenseValue);
+      setExpense(0);
+      setExpenseTitle("");
+      setEditIndex(null);
+      
+    }else{
+      setExpenseData((data)=>{
+        return [...data,{
+          title: expenseTitle,
+          amount: expenseValue,
+          Date: month+" "+year,
+          Time: new Date().toLocaleTimeString()
+        }]
+      })
       setTotalExpenditure(totalExpenditure + expenseValue);
-      setExpense("");
+      setExpense(0);
+      setExpenseTitle("");
+    
+    
+    console.log("Expense Data", expenseData);
     }
   };
 
-  console.log("Expense Title", expenseTitle);
+
 
   return (
     <View style={isdark ? styleSheet.darkStyle : styleSheet.lightStyle}>
@@ -280,9 +281,8 @@ const App = () => {
           width: "90%",
           alignItems: "center",
         }}
-        onPress={()=>{
-          console.log("yooo")
-        }}
+        disabled={expenseTitle === '' || expense === 0}
+        onPress = {()=>handleAddExpense()}
         >
           <Text style={{
             color: "white",
@@ -324,16 +324,29 @@ const App = () => {
           backgroundColor: "#009688",
           height:"100%",
           width:"90%",
+          minWidth:"90%",
           margin: 10,
           borderRadius: 10,
         }}>
+          
           {
+          expenseData.length != 0 ?
             expenseData.map((data, index) => {
               return (
                 <Expense data={data.title} amount={data.amount} date={data.Date} 
-                index={index} time={data.Time} key={index} handleEdit={handleEdit} />
+                index={index} time={data.Time} key={index} handleEdit={handleEdit} handleDelete= {handleDelete} />
               );
             }) 
+          :
+          <Text style={{
+            color:"white",
+            fontSize:20,
+            fontWeight:"bold",
+            textAlign:"center",
+            margin: 20
+          
+          }}> No Expense</Text>
+          
           }
 
         </View>
--- components/Expense.js
+++ components/Expense.js
@@ -4,7 +4,7 @@ import { Entypo } from "@expo/vector-icons";
 import { AntDesign } from "@expo/vector-icons";
 
 const Expense = (props) => {
-  const { amount, data, date,time , index , handleEdit} = props;
+  const { amount, data, date,time , index , handleEdit , handleDelete} = props;
   console.log(props)
   return (
     <View
@@ -43,7 +43,9 @@ const Expense = (props) => {
           <Entypo name="edit" size={20} color="white" />
           </TouchableOpacity>
 
-          <TouchableOpacity>
+          <TouchableOpacity onPress={()=>{
+            handleDelete(index)
+          }}>
           <AntDesign name="delete" size={20} color="white" />
 
           </TouchableOpacity>
@@ -71,3 +73,47 @@ const styles = StyleSheet.create({
   },
 });
 export default Expense;
+
+
+
+// {
+//   "title": "Clothes",
+//   "amount": 300,
+//   "Date": "May 30,2023",
+//   "Time": "3:29 PM"
+// },{
+//   "title": "Food",
+//   "amount": 400,
+//   "Date": "May 10,2023",
+//   "Time": "4:29 PM"
+// },{
+//   "title": "Electronics",
+//   "amount": 500,
+//   "Date": "May 12,2023",
+//   "Time": "5:29 PM"
+// },{
+//   "title": "Furniture",
+//   "amount": 600,
+//   "Date": "May 15,2023",
+//   "Time": "6:29 PM"
+// },{
+//   "title": "Books",
+//   "amount": 700,
+//   "Date": "May 17,2023",
+//   "Time": "7:29 PM"
+// },{
+//   "title": "Stationary",
+//   "amount": 800,
+//   "Date": "May 18,2023",
+//   "Time": "8:29 PM"
+// },{
+//   "title": "Shoes",
+//   "amount": 900,
+//   "Date": "May 19,2023",
+//   "Time": "9:29 PM"
+// },{
+//   "title": "Toys",
+//   "amount": 1000,
+//   "Date": "May 21,2023",
+//   "Time": "10:29 PM"
+// }
